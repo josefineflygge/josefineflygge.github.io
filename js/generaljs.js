0d2f5b2c0951ded2a8f2f4c7a25ac26900daf92a
Josefine Flügge <jflygge@gmail.com>
2019-02-05 20:35:59 +0100

    lot of small fixes

--- js/generaljs.js
+++ js/generaljs.js
@@ -1,22 +1,44 @@
 
 
-//LOADER
-var timer;
+/** LOADER **/
+    var timer;
 
-function loadPage() {
-    timer = setTimeout(showPage, 800);
-}
+    function loadPage() {
+        timer = setTimeout(showPage, 800);
+    }
 
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("loadable").style.visibility = "visible";
-}
+    function showPage() {
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("loadable").style.visibility = "visible";
+    }
 
-//Change background
+/** Change background on scroll **/
 
-$(function () {
-  $(document).scroll(function () {
-    var $bg = $("#bg");
-    $bg.toggleClass('scrolled', $(this).scrollTop() > 30);
+  $(function () {
+    $(document).scroll(function () {
+      var $bg = $("#bg");
+      $bg.toggleClass('scrolled', $(this).scrollTop() > 100);
+    });
   });
-});
+
+
+/** Change text color on scroll **/
+/*
+  $(function () {
+    $(document).scroll(function () {
+      var $font = $(".bigfont");
+      var $font2 =  $(".mediumfont");
+      $font.toggleClass('scrolled', $(this).scrollTop() > 80);
+      $font2.toggleClass('scrolled', $(this).scrollTop() > 80);
+    });
+  });
+*/
+  /** Scroll down button **/
+  // scrolldown - button, scrolltothis - section to scroll to,
+
+  $(function() {
+    $('.scroll-down').click (function() {
+      $('html,body, main').animate({scrollTop: $('.scrolltothis').offset().top }, 'slow');
+      return false;
+    });
+  });
